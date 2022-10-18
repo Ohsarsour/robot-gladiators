7b1f76ade6be9da8809e9a84ec88b9557c43bc3c
Ohsarsour <obaidullahsarsour@gmail.com>
2022-10-18 15:35:01 -0400

    checking player health

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,6 +11,7 @@ var enemyAttack = 12
 
 var fight = function () {
   // Alert players that they are starting the round
+
   window.alert('Welcome to Robot Gladiators!')
 
   //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
@@ -26,8 +27,32 @@ var fight = function () {
       enemyHealth +
       ' health remaining.'
   )
-  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+  // check enemy's health
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + ' has died!')
+  } else {
+    window.alert(enemyName + ' still has ' + enemyHealth + ' health left.')
+  }
+
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable
   playerHealth = playerHealth - enemyAttack
+
   // Log a resulting message to the console so we know that it worked.
-  console.log(playerHealth)
+  console.log(
+    enemyName +
+      ' attacked ' +
+      playerName +
+      '. ' +
+      playerName +
+      ' now has ' +
+      playerHealth +
+      ' health remaining.'
+  )
+  // check player's health
+  if (playerHealth <= 0) {
+    window.alert(playerName + ' has died!')
+  } else {
+    window.alert(playerName + ' still has ' + playerHealth + ' health left.')
+  }
 }
+fight()
